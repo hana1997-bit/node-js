edb3edd25d1740ff8c348a0641bd57de40e18ba6
idoudi hana <idoudihana06@gmail.com>
2021-08-07 12:41:00 +0100

    html

--- routes/mailApi.js
+++ routes/mailApi.js
@@ -24,8 +24,8 @@ const nodemailer = require("nodemailer");
     from: 'hanaidoudi88@gmail.com', // sender address
     to: "idoudihana06@gmail.com", // list of receivers
     subject: "Hello ✔", // Subject line
-    text: "Hello world?" // plain text body
-    // html: "<b>Hello world?</b>", // html body
+    // text: "Hello world?" // plain text body
+    html : "<b>bonjour</b>", // html body
   };
   Transporter.sendMail(mailoption , (err,info)=>{
       if(err){
