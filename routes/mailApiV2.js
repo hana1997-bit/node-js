e9b2424991f51af25d0db62f00995afd6a8e435b
idoudi hana <idoudihana06@gmail.com>
2021-08-14 10:10:32 +0100

    mailV2

--- routes/mailApiV2.js
+++ routes/mailApiV2.js
@@ -1,3 +1,46 @@
 const express = require('express');
 const router = express.Router();
-module.exports=router;
+const nodemailer = require("nodemailer");
+const path = require('path');
+const fs = require('fs');
+const ejs = require('ejs');
+
+router.post('/sendmailV2', async (req, res) => {
+    try {
+        //  1.0 create transporter
+        const Transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.MAIL, // generated ethereal user
+                pass: process.env.PASSWORD, // generated ethereal password
+            }
+        });
+        // 2.0 create mail option
+        // 2.1 read template path
+           const template_path = path.resolve('./mail_template' , 'register_notification.html');
+        //    console.log(template_path);
+        // 2.2 read template
+        const template = fs.readFileSync(template_path , {encoding : 'utf-8'});
+        //  console.log(template);
+        // 2.3 render template
+        const mailparameters ={
+            name : 'test1'
+        }
+        const render = ejs.render(template , mailparameters)
+        const mailoption = {
+            from: process.env.MAIL, // sender address
+            to: "idoudihana06@gmail.com, testforhamza@gmail.com", // list of receivers
+            subject: "Hello ✔", // Subject line
+            html: render, // html body
+        };
+        // 3.0 send mail
+        const info = await Transporter.sendMail(mailoption)
+        res.json({ message: 'check your mail' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'internal server error' });
+    }
+});
+
+
+module.exports = router;
