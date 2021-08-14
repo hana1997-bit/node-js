e37b3ced526060ef4b260232639ce09d749825f7
idoudi hana <idoudihana06@gmail.com>
2021-08-14 16:04:54 +0100

    first commit

--- routes/imagApi.js
+++ routes/imagApi.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const multer  = require('multer');
+const path = require('path')
+// 1.0 create storage
 
-router.get('/upload-image', (req, res) => {
-   res.render('index')
-
-})
+const my_storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+     cb(null, './public/avatars')
+   },
+   filename: function (req, file, cb) {
+   const file_extention=path.extname(file.originalname);
+     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)+file_extention
+     console.log(file_extention)
+     cb(null, file.fieldname + '-' + uniqueSuffix)
+   }
+ })
+// 2.0 create upload
+const upload = multer({ storage: my_storage })
+router.post('/upload-image', upload.single('file') ,(req, res) => {
+   res.json({message:'image uploaded successfully'});
+});
+router.post('/upload-image-multiple', upload.array('file',3) ,(req, res) => {
+   res.json({message:'images uploaded successfully'});
+});
 module.exports = router;
